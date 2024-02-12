153f82036892078c19daa09dbae27383cc5d88d7
evanknep <knepx001@umn.edu>
2024-02-11 23:52:05 -0600

    committing while my wide_tbl looks good

--- R/week4.R
+++ R/week4.R
@@ -1,3 +1,10 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-library(tidyr)
+library(tidyverse)
+
+#Data Import
+import_tbl <- read_delim("../data/week4.dat", delim = '-', col_names = c('casenum', 'parnum', 'stimver', 'datadate', 'qs'))
+summary(import_tbl)
+wide_tbl <- separate(data = import_tbl, col = "qs", into = c('q1', 'q2', 'q3', 'q4', 'q5')) 
+wide_tbl
+
